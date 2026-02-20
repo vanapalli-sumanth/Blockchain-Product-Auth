b171bc2f64f629553ec0609fc02002a9c0cfedd4
Surekha Pamulapati <surekhapamulapati3@gmail.com>
2026-02-21 02:13:19 +0530

    Fixed QR dynamic URL

--- utils/qr_generator.py
+++ utils/qr_generator.py
@@ -2,16 +2,23 @@ import qrcode
 import os
 from utils.token_generator import create_secure_token
 
-BASE_VERIFY_URL = "http://127.0.0.1:5000/verify"
+# ✅ Use environment variable, fallback to localhost
+BASE_VERIFY_URL = os.getenv(
+    "BASE_VERIFY_URL",
+    "http://127.0.0.1:5000/verify"
+)
 
 def generate_qr():
+
     token = create_secure_token()
+
     verify_url = f"{BASE_VERIFY_URL}?token={token}"
 
     folder = "static/qr_codes"
     os.makedirs(folder, exist_ok=True)
 
     path = f"{folder}/{token}.png"
+
     qrcode.make(verify_url).save(path)
 
     return {
